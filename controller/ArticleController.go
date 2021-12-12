f363d9434b35c2fe5d42c2ae6401cdba7b517253
teo <ferizovic.teo@gmail.com>
2021-12-12 17:20:01 +0100

    Search articles by params

--- controller/ArticleController.go
+++ controller/ArticleController.go
@@ -8,8 +8,21 @@ import (
 
 func ArticleIndex(ctx *gin.Context) {
 
+	id := ctx.Query("id")
+	userId := ctx.Query("userId")
 
-	err, resultArticles := model.GetArticles()
+	resultArticles := []model.Article{}
+	err := *new(error)
+
+	if id != "" && userId == "" {
+		err, resultArticles = model.GetArticleById(id)
+	} else if userId != "" && id == "" {
+		err, resultArticles = model.GetArticleByUserId(userId)
+	} else if  id != "" && userId != "" {
+		err, resultArticles = model.GetArticleByIdAndUserId(id,userId)
+	} else {
+		err, resultArticles = model.GetArticles()
+	}
 
 	if err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{
--- model/ArticleModel.go
+++ model/ArticleModel.go
@@ -34,4 +34,53 @@ func GetArticles() (error error, articles []Article) {
 
 }
 
+//find Article by id
+func GetArticleById(id string) (error error, articles []Article) {
 
+	var resultArticles []Article
+
+	err := database.DBCon.Preload("User").Where("id = ?", id).Find(&resultArticles).Error
+
+	errors.Is(err, gorm.ErrRecordNotFound)
+
+	if err != nil {
+		return err, resultArticles
+	}
+
+	return nil, resultArticles
+
+}
+
+//find Article by user_id
+func GetArticleByUserId(userId string) (error error, articles []Article) {
+
+	var resultArticles []Article
+
+	err := database.DBCon.Preload("User").Where("user_id = ?", userId).Find(&resultArticles).Error
+
+	errors.Is(err, gorm.ErrRecordNotFound)
+
+	if err != nil {
+		return err, resultArticles
+	}
+
+	return nil, resultArticles
+
+}
+
+//find Article by user_id
+func GetArticleByIdAndUserId(id string, userId string) (error error, articles []Article) {
+
+	var resultArticles []Article
+
+	err := database.DBCon.Preload("User").Where("id = ? AND user_id = ?", id, userId).Find(&resultArticles).Error
+
+	errors.Is(err, gorm.ErrRecordNotFound)
+
+	if err != nil {
+		return err, resultArticles
+	}
+
+	return nil, resultArticles
+
+}
